faee61c1f9844b7178efd6d5499dc029aa96059e
bcarneiro08@gmail.com <bcarneiro08@gmail.com>
2019-04-22 21:31:18 -0300

    Teste10

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function teste(texto){
-    alert(texto);
+function sexo(){
+    if (btn-masculino 'true'){
+        valor = formGroupPeso / (formGroupAltura*2);
+        valor = formGroupPeso / valor;
+        alert = valor;
+    }else
+    if (btn-feminino 'true'){
+        valor = formGroupPeso / (formGroupAltura*2);
+        valor = formGroupPeso / valor;
+        alert = valor;
+    }else
 }
